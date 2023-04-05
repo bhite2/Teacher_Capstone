96b36c0bdd67303665ca699d90850c02aca07595
Bryan Hite <bhite2@mail.usf.edu>
2023-04-05 16:04:17 -0400

    Created the model for comments.

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
+
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource_id = models.CharField(max_length=30)
+    text = models.CharField(max_length=100)
