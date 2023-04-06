ccff4f247969c1ef29522d23a2cdb3923f514f33
Bryan Hite <bhite2@mail.usf.edu>
2023-04-06 13:35:44 -0400

    Updated resources model + created serializer

--- backend/resources/models.py
+++ backend/resources/models.py
@@ -4,9 +4,9 @@ from gradelevels.models import GradeLevel
 # Create your models here.
 
 class Resource(models.Model):
-    name = models.CharField(max_length=255)
-    alter_ego = models.CharField(max_length=255)
-    primary_ability = models.CharField(max_length=255)
-    secondary_ability = models.CharField(max_length=255)
-    catch_phrase = models.CharField(max_length=255)
-    grade_level = models.ManyToManyField(GradeLevel, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    subject = models.CharField(max_length=255)
+    grade_level = models.ManyToManyField(GradeLevel)
+    file = models.FileField(upload_to=None, max_length=255)
+    
