957ff6db00cc345b24c8910cae6d56706f2fef8e
Bryan Hite <bhite2@mail.usf.edu>
2023-04-10 10:01:20 -0400

    Fixed error with resources views.py

--- backend/resources/views.py
+++ backend/resources/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Resource
+from .serializer import ResourceSerializer
 
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def get_all_resources(request):
+    if request.method == 'GET':
+        resources = Resource.objects.all()
+        serializer = ResourceSerializer(resources, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ResourceSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
