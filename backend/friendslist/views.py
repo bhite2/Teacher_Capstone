24f92a1e1d81af86d8010d62ce18d209465d18df
Bryan Hite <bhite2@mail.usf.edu>
2023-04-19 15:23:21 -0400

    Created CreateResource folder for frontend.

--- backend/friendslist/views.py
+++ backend/friendslist/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from authentication.models import User
-from .serializer import FriendsListSerializer
+from authentication.serializers import RegistrationSerializer
+from .models import AddFriend
+from .serializer import AddFriendSerializer
 
 # Create your views here.
 
@@ -13,13 +15,13 @@ from .serializer import FriendsListSerializer
 @permission_classes([IsAuthenticated])
 def get_all_users(request):
     users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
+    serializer = RegistrationSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def add_friend(request):
-    serializer = FriendsListSerializer(data=request.data)
+    serializer = AddFriendSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
